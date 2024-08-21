ee12b8a92be0fd46304c10ab2a47d9aeebb4d72b
atiqur rahman <abulbasharatiqur@gmail.com>
2024-08-21 17:35:29 +0600

    poppins font added

--- app/components/Comments.js
+++ app/components/Comments.js
@@ -1,16 +1,16 @@
 export default async function Comments({ promise }) {
-    const comments = await promise;
+  const comments = await promise;
 
-    return (
-        <div className="mt-10">
-            <h1>Comments</h1>
-            <ul>
-                {comments.map((comment) => (
-                    <li className="mb-2" key={comment.id}>
-                        {comment.body}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="mt-10">
+      <h1 className="text-2xl">Comments</h1>
+      <ul>
+        {comments.map((comment) => (
+          <li className="mb-2" key={comment.id}>
+            {comment.body}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
